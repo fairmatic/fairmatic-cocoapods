e632d3e122a4492d561c247809b0a8563e8c1232
Sagar Dagdu <shags032@gmail.com>
2024-10-24 14:39:55 +0530

    Try out SQLite binary xcframework

---
+++ FairmaticFrameworks/FairmaticSDK.xcframework/ios-arm64_x86_64-simulator/FairmaticSDK.framework/Headers/Fairmatic.h
@@ -0,0 +1,18 @@
+//
+//  Fairmatic.h
+//  Fairmatic
+//
+//  Created by Sagar Dagdu on 24/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fairmatic.
+FOUNDATION_EXPORT double FairmaticVersionNumber;
+
+//! Project version string for Fairmatic.
+FOUNDATION_EXPORT const unsigned char FairmaticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fairmatic/PublicHeader.h>
+
+
--- SQLite.xcframework/ios-arm64/SQLite.framework/Headers/SQLite.h
+++
@@ -1,4 +0,0 @@
-@import Foundation;
-
-FOUNDATION_EXPORT double SQLiteVersionNumber;
-FOUNDATION_EXPORT const unsigned char SQLiteVersionString[];
